ba233444b5e17d05e4b269a640e8a7ba2dc047c8
EdilKulzhabay <edil.kulzhabay01@gmail.com>
2025-01-27 16:14:06 +0500

    27.01.2025 change model gpt

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ const gptResponse = async (text) => {
     const response = await axios.post(
         "https://api.openai.com/v1/chat/completions",
         {
-            model: "gpt-3.5-turbo",
+            model: "gpt-4o-mini",
             messages,
         },
         {
